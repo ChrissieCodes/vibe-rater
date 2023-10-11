162a9e79b2b0cfdc2154a20e89142e9956c9a844
Chrissie Codes <chrissiecodes@gmail.com>
2023-10-11 05:48:56 +0000

    added prefix to endpoint

--- sentiment analysis/vibes/main.py
+++ sentiment analysis/vibes/main.py
@@ -15,38 +15,38 @@ app = Flask(__name__)
 db = SessionLocal()
 
 
-@app.route("/totals/", methods=["GET"])
+@app.route("/vibe-rater/totals/", methods=["GET"])
 def total_sentiment():
     return [x.to_dict() for x in repository.get_totals_by_username(db=db)]
 
 
-@app.route("/sentiment/", methods=["POST"])
+@app.route("/vibe-rater/sentiment/", methods=["POST"])
 def new_sentiment():
     return repository.create_sentiment(db=db, sentiment=request.json).to_dict()
 
 
-@app.route("/sentiment/", methods=["GET"])
+@app.route("/vibe-rater/sentiment/", methods=["GET"])
 def read_sentiment():
     return [sentiment.to_dict() for sentiment in repository.get_sentiment(db)]
 
 
-@app.route("/sentiment/", methods=["POST"])
+@app.route("/vibe-rater/sentiment/", methods=["POST"])
 def updated_sentiment():
     db_sentiment = repository.update_sentiment(db, sentiment=request.json)
     return db_sentiment
 
-@app.route("/", methods=["GET"])
+@app.route("/vibe-rater/", methods=["GET"])
 def root():
     return {"message": "welcome to my vibe rater"}
 
 
-@app.route("/chat/", methods=["POST"])
+@app.route("/vibe-rater/chat/", methods=["POST"])
 def add_chat():
     chat_rating = vibe_rater(request.json["message"])
     return chat_rating
 
 
-@app.route("/serial/", methods=["GET"])
+@app.route("/vibe-rater/serial/", methods=["GET"])
 def run_bubbles():
     with serial.Serial(
         "COM4", 115200, bytesize=8, parity="N", stopbits=1, timeout=1
